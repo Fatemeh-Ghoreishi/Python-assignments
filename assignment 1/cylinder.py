bd256a1c2f42bebafa9390887c492ec3755580cd
Fatemeh-Ghoreishi <110478506+Fatemeh-Ghoreishi@users.noreply.github.com>
2022-08-13 19:06:27 +0430

    Update cylinder.py

--- assignment 1/cylinder.py
+++ assignment 1/cylinder.py
@@ -1,12 +1,10 @@
 radius = int(input("please enter radius of the cylinder: "))
 height = int(input("please enter height of the cylinder: "))
 pi = 3.14
-area_of_circle = (pi*radius*radius)
-perimeter_of_circle = (2*pi*radius)
 
-side_area = (perimeter_of_circle*height)
-total_area = ((side_area)+(2*area_of_circle))
-volume = (area_of_circle*height)
+side_area = (2*pi*radius*height)
+total_area = ((side_area)+(2*pi*radius*radius))
+volume = (pi*radius*radius*height)
 
 print("side_area:",side_area)
 print("total_area:" ,total_area)
