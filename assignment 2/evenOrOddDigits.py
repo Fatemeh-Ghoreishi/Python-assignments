0c7f863186fd7044cc19b719526ce833de8a8cf6
Fatemeh-Ghoreishi <110478506+Fatemeh-Ghoreishi@users.noreply.github.com>
2022-08-21 13:04:33 +0430

    Update evenOrOddDigits.py

--- assignment 2/evenOrOddDigits.py
+++ assignment 2/evenOrOddDigits.py
@@ -1,16 +1,16 @@
-count = int(input('How many digits is your desired number? '))
 number = int(input('Enter your number: '))
 even = 0
 odd = 0
 
-while count > 0 :
+while number != 0 :
     new_number = int(number % 10)
     number = int(number / 10)
     if (new_number % 2 == 0):
         even = even + 1
-    else:
+    elif (new_number % 2 != 0):
         odd = odd + 1
-    count = count - 1
+    else:
+        break
 
 if (even > odd):
     print('The number of even digits is more.')
