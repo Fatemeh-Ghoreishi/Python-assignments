7f346bc3d45ae9b5bc9d6ad67d8e1edffff7a8bb
Fatemeh-Ghoreishi <110478506+Fatemeh-Ghoreishi@users.noreply.github.com>
2022-09-16 17:56:25 +0430

    Add files via upload

---
+++ assignment 5/math_operation.py
@@ -0,0 +1,45 @@
+def math(st):
+    op = ['+','-' ,'*','/']
+    oprs = []
+    for i in st:
+        for j in range(4):
+            if i == op[j]:
+                oprs.append(j)
+    for i in range(4):
+        math = st.split(op[i])
+        st = ' '.join(math)
+    math = st.split(' ')
+    while '' in math:
+        math.remove('')
+    for i in range(len(math)):
+        math[i] = int(math[i])
+    count = 0
+    while '*' in oprs or '/' in oprs:
+        if oprs[count] == '*':
+            math[count] = math[count] * math[count+1]
+            math.pop(count+1)
+            oprs.pop(count)
+            count -= 1
+        elif oprs[count] == '/':
+            math[count] = math[count] / math[count+1]
+            math.pop(count+1)
+            oprs.pop(count)
+            count -= 1
+        count += 1
+    count = 0
+    while '+' in oprs or '-' in oprs:
+        if oprs[count] == '+':
+            math[count] = math[count] + math[count+1]
+            math.pop(count+1)
+            oprs.pop(count)
+            count -= 1
+        elif oprs[count] == '-':
+            math[count] = math[count] - math[count+1]
+            math.pop(count+1)
+            oprs.pop(count)
+            count -= 1
+        count += 1
+    count = 0
+    print(math[0])
+st = input('Enter your math expression: ')
+math(st)
